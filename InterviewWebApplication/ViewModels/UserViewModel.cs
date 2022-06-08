0caa6299c48789769ccae2c40330c5434352a0da
KevinStoneTW <kwshih0212@gmail.com>
2022-06-08 17:14:30 +0800

    新增dapper套件，新增User Model與ViewModel

---
+++ InterviewWebApplication/ViewModels/UserViewModel.cs
@@ -0,0 +1,13 @@
+﻿using InterviewWebApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace InterviewWebApplication.ViewModels
+{
+    public class UserViewModel
+    {
+        public IEnumerable<User> Users { get; set; }
+    }
+}
