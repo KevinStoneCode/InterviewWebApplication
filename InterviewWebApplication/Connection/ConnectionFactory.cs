ca1f07310f308459e0a098f4a5447bb3df877360
KevinStoneTW <kwshih0212@gmail.com>
2022-06-08 16:52:30 +0800

    新增連線方法

---
+++ InterviewWebApplication/Connection/ConnectionFactory.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace InterviewWebApplication.Connection
+{
+    public class ConnectionFactory
+    {
+        public IDbConnection CreateConnection(string name = "default")
+        {
+            switch (name)
+            {
+                case "default":
+                    {
+                        var ConnectionString = System.Web.Configuration.WebConfigurationManager.ConnectionStrings["db_main"].ConnectionString;
+
+                        return new SqlConnection(ConnectionString);
+                    }
+                default:
+                    {
+                        throw new Exception("name 不存在。");
+                    }
+            }
+        }
+    }
+}
--- InterviewWebApplication/Controllers/HomeController.cs
+++ InterviewWebApplication/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using InterviewWebApplication.Connection;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,6 +9,13 @@ namespace InterviewWebApplication.Controllers
 {
     public class HomeController : Controller
     {
+        private ConnectionFactory _connectionFactory;
+
+        public HomeController()
+        {
+            _connectionFactory = new ConnectionFactory();
+        }
+
         public ActionResult Index()
         {
             return View();
