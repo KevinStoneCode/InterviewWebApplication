d6466137cedba6bc9f109c99fdbb17c5f6aa26c6
KevinStoneTW <kwshih0212@gmail.com>
2022-06-09 14:29:22 +0800

    新增"新增資料"功能

--- InterviewWebApplication/Controllers/HomeController.cs
+++ InterviewWebApplication/Controllers/HomeController.cs
@@ -43,6 +43,34 @@ namespace InterviewWebApplication.Controllers
             return View(viewModel);
         }
 
+        public ActionResult Create()
+        {
+            var viewModel = new UserFormViewModel();
+            
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(UserFormViewModel viewModel)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return View(viewModel);
+                }
+
+                CreateUser(viewModel.User);
+            }
+            catch(Exception ex)
+            {
+
+            }
+            
+            return RedirectToAction("Index");
+        }
+
         private IEnumerable<User> GetUsers()
         {
             var sql = @"
@@ -70,6 +98,30 @@ namespace InterviewWebApplication.Controllers
             {
                 var user = conn.QuerySingleOrDefault<User>(sql,p);
                 return user;
+            }
+
+        }
+
+        private User CreateUser(User user)
+        {
+            var sql = @"
+                    INSERT INTO Users (
+                        Name, Email
+                    )
+                    OUTPUT INSERTED.Id
+                    VALUES (
+                        @Name, @Email
+                    )
+                    ";
+
+            DynamicParameters p = new DynamicParameters();
+            p.Add("@Name", user.Name);
+            p.Add("@Email", user.Email);
+
+            using (var conn = _connectionFactory.CreateConnection())
+            {
+                var insertedUser = conn.QuerySingleOrDefault<User>(sql, p);
+                return insertedUser;
             }
 
         }
